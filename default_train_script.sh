74aba12a454e47db263c8e9bd0a6f8a4196e7ed0
dkorzh10 <dkorzh10@gmail.com>
2025-04-08 04:18:48 +0300

    changed parameters to make sense

--- default_train_script.sh
+++ default_train_script.sh
@@ -3,6 +3,6 @@ CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python main.py train \
     --dataset_path big_data \
     --pretrain_model sentence-transformers/all-MiniLM-L6-v2 \
     --learning_rate 0.00001 \
-    --batch_size 16 \
-    --n_epochs 5 \
+    --batch_size 32 \
+    --n_epochs 10 \
     --report_to wandb
